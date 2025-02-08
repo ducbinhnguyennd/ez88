9bde25ab4361510ade85fc66e4099a18824cb00c
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2025-02-08 16:01:02 +0700

    update

---
+++ src/router/router.js
@@ -0,0 +1,9 @@
+import { TrangChuLayout } from '../Layout/TrangChuLayout'
+
+
+const publicRoutes = [
+  { path: '/', component: TrangChuLayout },
+
+]
+const privateRoutes = []
+export { publicRoutes, privateRoutes }
